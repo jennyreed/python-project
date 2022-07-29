e52ad05b546fc3efdac63cc03bccc77f0ac3bdbe
jennyreed <reedjenny656@gmail.com>
2022-07-29 15:27:08 +1200

    mhmmmhm

--- bubble_test.py
+++ bubble_test.py
@@ -1,22 +1,26 @@
 from random import randint, random
-from bubblesorter import bubbleSort
+
+from numpy import average
+from bubblesorter import bubbleSort, arr
 from time import time
- 
-averagecase= [randint(1,10000) for _ in range(1000)]
+
+averagecase = arr
+#averagecase= [randint(1,10000) for _ in range(1000)]
 bestcase= sorted(averagecase)
 worstcase= sorted(averagecase, reverse=True)
- 
+
+
 def test_BubbleWorst():
     start= time()
-    assert bubbleSort(worstcase) == bestcase
+    assert bubbleSort(list.copy(worstcase)) == bestcase
     print(time()-start)
  
 def test_BubbleBest():
     start= time()
-    assert bubbleSort(bestcase) == bestcase
+    assert bubbleSort(list.copy(bestcase)) == bestcase
     print(time()-start)
  
 def test_BubbleAvg():
     start= time()
-    assert bubbleSort(averagecase) == bestcase
+    assert bubbleSort(list.copy(averagecase)) == bestcase
     print(time()-start)
--- bubblesorter.py
+++ bubblesorter.py
@@ -1,31 +1,43 @@
 import time
 startTime = time.time()
-array = []
+arr = []
 
 def bubbleSort(arr):
 
-    swapped = False
+    # swapped = False
 
-    for n in range(len(arr)-1, 0, -1):
-        for i in range(n):
-            if arr[i] < arr[i + 1]:
-                swapped = True
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    # for n in range(len(arr)-1, 0, -1):
+    #     for i in range(n):
+    #         if arr[i] < arr[i + 1]:
+    #             swapped = True
+    #             arr[i], arr[i + 1] = arr[i + 1], arr[i]
 
-            if not swapped:
-                return
+    #         if not swapped:
+    #             return arr
+    # return arr
 
-    print('\nSorted Array:\n')
-    print(array)
-    bubbleSort(list.copy(array))
+    while(sorted(arr) != arr):
+        # Iterate from 0 to the length of the arr - 1
+        for i in range(len(arr) - 1):
+            # If the current item is greater than the next item, swap the items
+            if(arr[i] > arr[i + 1]):
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+    # Return the sorted array
+    return arr
 
 fileObj = open('data2.txt', 'r')
-array = fileObj.read().splitlines()
+arr = fileObj.read().splitlines()
 fileObj.close()
-print('Unsorted Array\n')
-print(array)
-
-bubbleSort(array)
+listCopy = list.copy(arr)
+bubbleSort(listCopy)
+print('Unsorted arr\n')
+print(arr)
+bubbleSort(arr)
+print('\nSorted arr:\n')  
+print(arr)
 
 executeTime = (time.time() - startTime)
 print('Time taken to sort: ' + str(executeTime))
+
