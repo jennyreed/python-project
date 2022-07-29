f293873ca9c55eb29c868a72271ebe6e4e496e37
jennyreed <reedjenny656@gmail.com>
2022-07-29 15:32:00 +1200

    add s select_test

---
+++ select_test.py
@@ -0,0 +1,25 @@
+from random import randint, random
+from numpy import average
+from selectionsorter import selecsort, array
+from time import time
+
+averagecase = array
+#averagecase= [randint(1,10000) for _ in range(1000)]
+bestcase= sorted(averagecase)
+worstcase= sorted(averagecase, reverse=True)
+
+
+def test_SelectWorst():
+    start= time()
+    assert selecsort(list.copy(worstcase)) == bestcase
+    print(time()-start)
+ 
+def test_SelectBest():
+    start= time()
+    assert selecsort(list.copy(bestcase)) == bestcase
+    print(time()-start)
+ 
+def test_SelectAvg():
+    start= time()
+    assert selecsort(list.copy(averagecase)) == bestcase
+    print(time()-start)
